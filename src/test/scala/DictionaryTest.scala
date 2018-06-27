819ac3ff050b2745781af80cddeadd5e0f7d4500
zzawadz <zygmunt@zstat.pl>
2018-06-27 19:09:46 +0200

    update dictionary tests to loremipsum model

--- src/test/scala/DictionaryTest.scala
+++ src/test/scala/DictionaryTest.scala
@@ -6,16 +6,16 @@ class DictionaryTest extends FlatSpec {
 
   "Parameters" should "match predefined values" in {
 
-    val filePath = "data/wiki.pl/wiki.pl.bin"
+    val filePath = "data/loremipsum/loremModel.bin"
     val reader = new BinaryReader(filePath)
     val args   = new Args(reader)
     val dict   = new Dictionary(args, reader)
 
     val wordsIdxs = dict.getSubwords("scala")
 
-    val target : Array[Int] = Array(32937, 1828514, 2069569, 3026629, 1748544, 2157591, 1229711,
-    1084350, 2622266, 1626048, 2536187, 2143885, 1690100, 1269816,
-    1523105)
+    val target : Array[Int] = Array(
+      796124, 1037179, 1994239, 716154, 1125201, 197321,
+      51960, 1589876, 593658, 1503797, 1111495, 657710, 237426, 490715)
 
     assert(wordsIdxs.zip(target).forall{case (x, y) => x == y})
 
